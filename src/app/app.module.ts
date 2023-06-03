d23d3b0cb43b3b0e5e1091a80614bc68777e66d0
Caroline <wanguic49@gmail.com>
2023-06-03 23:18:02 +0300

    few  changes made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { SideNavComponent } from './main/side-nav/side-nav.component';
+import { ContentNavComponent } from './main/content-nav/content-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    SideNavComponent,
+    ContentNavComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/main/content-nav/content-nav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-nav',
+  templateUrl: './content-nav.component.html',
+  styleUrls: ['./content-nav.component.css']
+})
+export class ContentNavComponent {
+
+}
