faaf21086257217dc62653ead648d4ac8fb6b47f
Caroline <wanguic49@gmail.com>
2023-06-09 17:17:37 +0300

    updated routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { SideNavComponent } from './main/side-nav/side-nav.component';
 import { ContentNavComponent } from './main/content-nav/content-nav.component';
+
+import { RouterModule,Routes } from '@angular/router';
+
+const routes:Routes = [
+  
+];
 
 @NgModule({
   declarations: [
@@ -14,6 +20,7 @@ import { ContentNavComponent } from './main/content-nav/content-nav.component';
     ContentNavComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule
   ],
   providers: [],
